74aa850a62f20333667ea6383eff356bca252eaf
Olivier Lamy <olamy@apache.org>
2025-02-21 16:59:58 +1000

    Use -DdeplotAtEnd to avoid partial deployment for multi module which fail subsequent retries
    see https://github.com/jenkins-infra/helpdesk/issues/4549

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ then
     gh api /repos/$GITHUB_REPOSITORY/releases | jq -e -r '[ .[] | select(.draft == true and .name == "next")] | max_by(.id).body' | egrep "$INTERESTING_CATEGORIES"
 fi
 export MAVEN_OPTS=-Djansi.force=true
-mvn -B -V -e -s $GITHUB_ACTION_PATH/settings.xml -ntp -Dstyle.color=always -Dset.changelist -DaltDeploymentRepository=maven.jenkins-ci.org::default::https://repo.jenkins-ci.org/releases/ -Pquick-build -P\!consume-incrementals clean deploy
+mvn -B -V -e -s $GITHUB_ACTION_PATH/settings.xml -ntp -Dstyle.color=always -Dset.changelist -DaltDeploymentRepository=maven.jenkins-ci.org::default::https://repo.jenkins-ci.org/releases/ -Pquick-build -P\!consume-incrementals clean deploy -DdeployAtEnd=true
 version=$(mvn -B -ntp -Dset.changelist -Dexpression=project.version -q -DforceStdout help:evaluate)
 # Create the annotated git tag - https://docs.github.com/en/rest/git/tags#create-a-tag-object
 gh api -F tag=$version -F message=$version -F object=$GITHUB_SHA -F type=commit /repos/$GITHUB_REPOSITORY/git/tags
